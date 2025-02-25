915fde3712a5ad25fd726998e55e44dde1a5d224
evanng07 <ngoeva072@gmail.com>
2025-02-24 20:41:38 -0600

    first commit

---
+++ Backend/util/location.js
@@ -0,0 +1,31 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const API_KEY = process.env.API_KEY;
+const HttpError = require("../models/http-error");
+
+const axios = require("axios");
+
+async function getCoordsForAddress(address) {
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address",
+      422
+    );
+
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
